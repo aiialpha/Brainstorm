57efd1a74bd823ff140d7e8e6ef1ee2c8a1e721f
aiialpha <aii07038713563@gmail.com>
2023-10-30 13:44:17 +0100

    animations added

--- js/scripti.js
+++ js/scripti.js
@@ -12,3 +12,25 @@ let navHead = document.querySelector("#nav")
    navmenu.classList.toggle("open");
    navHead.classList.toggle("color")
    };
+   // Get the element you want to animate
+const element = document.querySelector('.animate-me');
+
+// Function to add the 'animate' class when the element is in the viewport
+function handleIntersection(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      element.classList.add('animate');
+      observer.unobserve(element); // Stop observing once animation is triggered
+    }
+  });
+}
+// Create an Intersection Observer
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.2, // Adjust this value to control when the animation triggers
+};
+const observer = new IntersectionObserver(handleIntersection, options);
+
+// Start observing the element
+observer.observe(element);
